36fb76ff48384b7da9480869b4d4157d919ce36a
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-07 21:46:22 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaCondicional/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Course{
+    class Program {
+        static void Main(string[] args) {
+
+            int horas;
+
+            Console.Write("Digite o horário:");
+            horas = int.Parse(Console.ReadLine());
+
+            if (horas < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (horas < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite!");
+
+            }
+
+             Console.ReadLine();
+
+        }
+    }
+}
